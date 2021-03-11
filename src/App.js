8b20b52c7ea48d96bba091a05dfeb36aa01927a5
jimmymendy <jimmymendy@Pros-MacBook-Pro.local>
2021-03-11 15:11:23 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { 
+  Typography, 
+  AppBar, 
+  Button,
+  Card, 
+  CardActions, 
+  CardContent, 
+  CardMedia, 
+  CssBaseline, 
+  Grid, 
+  Toolbar, 
+  Container 
+} from '@material-ui/core';
+import {PhotoCamera} from '@material-ui/icons';
 
-function App() {
+//Styles
+import useStyles from './styles';
+
+
+//cards
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//Footer
+const Copyright = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer
+      </Typography>
+      <Typography variant="subtitle1" align="center" color="textSecondary">
+       copyright © {new Date().getFullYear()}
+      </Typography>
+    </>
+  )
+}
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph> 
+              Hello everyone This is a photo album and I'm trying to make this enstence as long as possible so we see how does it look like on the screen
+            </Typography> 
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((i) => (
+              <Grid item key={i} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia 
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                        Heading
+                    </Typography>
+                    <Typography>
+                        This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">View</Button>
+                    <Button size="small" color="primary"></Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+          {Copyright()}
+      </footer>
+        
+    </>
   );
 }
 
